44aca275f9e3c658056f65cbbee444ca9df7f689
haixing1001 <50311834+haixing1001@users.noreply.github.com>
2025-03-23 05:40:12 +0800

    Update build.sh
    Signed-off-by: haixing1001 <50311834+haixing1001@users.noreply.github.com>

--- build.sh
+++ build.sh
@@ -55,20 +55,3 @@ if [[ $Build_Mod == "debug" ]]; then
     exit 0
 fi
 
-TARGET_DIR="$BASE_PATH/$BUILD_DIR/bin/targets"
-if [[ -d $TARGET_DIR ]]; then
-    find "$TARGET_DIR" -type f \( -name "*.bin" -o -name "*.manifest" -o -name "*efi.img.gz" -o -name "*.itb" -o -name "*.fip" -o -name "*.ubi" -o -name "*rootfs.tar.gz" \) -exec rm -f {} +
-fi
-
-make download -j$(($(nproc) * 2))
-make -j$(($(nproc) + 1)) || make -j1 V=s
-
-FIRMWARE_DIR="$BASE_PATH/firmware"
-\rm -rf "$FIRMWARE_DIR"
-mkdir -p "$FIRMWARE_DIR"
-find "$TARGET_DIR" -type f \( -name "*.bin" -o -name "*.manifest" -o -name "*efi.img.gz" -o -name "*.itb" -o -name "*.fip" -o -name "*.ubi" -o -name "*rootfs.tar.gz" \) -exec cp -f {} "$FIRMWARE_DIR/" \;
-\rm -f "$BASE_PATH/firmware/Packages.manifest" 2>/dev/null
-
-if [[ -d $BASE_PATH/action_build ]]; then
-    make clean
-fi
